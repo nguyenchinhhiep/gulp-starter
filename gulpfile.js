744da466cd33729ac1846fe8da8c9ddd6fd365ad
nguyenchinhhiep <nguyenchinhhiep95@gmail.com>
2020-03-23 12:43:10 +0700

    fix browser sync issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,77 @@
-const { src, dest, series, parallel, watch } = require('gulp');
-const sass = require('gulp-sass');
-const babel = require('gulp-babel');
-const pug = require('gulp-pug');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
-const del = require('del');
-const runSequence = require('run-sequence');
+const { src, dest, series, parallel, watch } = require("gulp");
+const sass = require("gulp-sass");
+const babel = require("gulp-babel");
+const pug = require("gulp-pug");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+const del = require("del");
 const reload = browserSync.reload;
 
-const sassTask = function () {
-    return src('src/sass/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 99 version'],
-            cascade: false
-        }))
-        .pipe(dest('dist/css'))
-        .pipe(browserSync.reload({
-            stream: true
-          }))
+const sassTask = function() {
+  return src("src/sass/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 99 version"],
+        cascade: false
+      })
+    )
+    .pipe(dest("dist/css"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 };
 
-const jsTask = function () {
-    return src('src/js/**/*.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(dest('dist/js'))
+const jsTask = function() {
+  return src("src/js/**/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(dest("dist/js"));
 };
 
-const pugTask = function () {
-    return src('src/view/pages/*.pug')
-    .pipe(pug({
+const pugTask = function() {
+  return src("src/view/pages/*.pug")
+    .pipe(
+      pug({
         pretty: true
-    }))
-    .pipe(dest('dist/html'))
+      })
+    )
+    .pipe(dest("dist/html"));
 };
 
-const assetsTask = function () {
-    return src('src/assets/**/*')
-    .pipe(dest('dist/assets/'))
-}
+const assetsTask = function() {
+  return src("src/assets/**/*").pipe(dest("dist/assets/"));
+};
 
-const delTask = async function(){
-    return del.sync('dist');
-}
+const delTask = async function() {
+  return del.sync("dist");
+};
 
 function watchTask() {
-    browserSync.init({
-        server: {
-          baseDir: 'dist/html'
-        },
-      })
-    watch(['src/js/**/*.js', 'src/sass/**/*.scss', 'src/view/**/*.pug', 'src/assets/**/*'],
-        parallel(sassTask, jsTask, pugTask, assetsTask));
-    watch('./dist/html/*.html').on('change', reload);
-    watch('./dist/js/*.js').on('change', reload);
-    watch('.dist/assets/**/*').on('change', reload);
-};
+  browserSync.init({
+    server: {
+      baseDir: "dist",
+      index: "html/index.html"
+    }
+  });
+  watch(
+    [
+      "src/js/**/*.js",
+      "src/sass/**/*.scss",
+      "src/view/**/*.pug",
+      "src/assets/**/*"
+    ],
+    parallel(sassTask, jsTask, pugTask, assetsTask)
+  );
+  watch("src/view/**/*.pug").on("change", reload);
+  watch("src/js/**/*.js").on("change", reload);
+  watch("src/assets/**/*").on("change", reload);
+}
 
 exports.sassTask = sassTask;
 exports.jsTask = jsTask;
@@ -66,4 +80,8 @@ exports.assetsTask = assetsTask;
 exports.watchTask = watchTask;
 exports.delTask = delTask;
 
-exports.default = series(delTask, parallel(jsTask, sassTask, pugTask, assetsTask),watchTask);
+exports.default = series(
+  delTask,
+  parallel(jsTask, sassTask, pugTask, assetsTask),
+  watchTask
+);
